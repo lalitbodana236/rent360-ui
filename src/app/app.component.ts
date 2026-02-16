84be9543ed730f8755421c226074243977f350d1
lalitbodana236 <lalitbodana236@gmail.com>
2026-02-16 23:43:18 +0530

    name configuration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,13 @@
 import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { environment } from '../environments/environment';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
 })
-export class AppComponent {}
+export class AppComponent {
+  constructor(private readonly title: Title) {
+    this.title.setTitle(environment.branding.appName);
+  }
+}
